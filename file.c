a9e951737907621fbdc1c229a339a163d575e906
abderrahmane <abderrahamane7@gmail.com>
2021-11-18 23:48:32 +0100

    tweaked the visuals

--- file.c
+++ file.c
@@ -38,16 +38,18 @@ void printConds(struct condidat *arr, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		printf("%d -> %s %s %s\n", i, arr[i].fname, arr[i].lname, arr[i].cin);
+		printf("** %d -> %s %s %s\n", i + 1, arr[i].fname, arr[i].lname, arr[i].cin);
 	}
+	printf("\n");
 }
 
 void printElects(struct electeur *arr, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		printf("%d -> %s\n", i + 1, arr[i].cin);
+		printf("** %d -> %s\n", i + 1, arr[i].cin);
 	}
+	printf("\n");
 }
 
 void main()
@@ -67,7 +69,7 @@ void main()
 			electeurs = (struct electeur *)malloc(elecSize * sizeof(struct electeur));
 			for (int i = 0; i < elecSize; i++)
 			{
-				printf("entrer cin %d : ", i);
+				printf("entrer cin %d : ", i + 1);
 				scanf("%s", &electeurs[i].cin);
 			}
 		}
@@ -83,19 +85,20 @@ void main()
 			condidats = (struct condidat *)malloc(condSize * sizeof(struct condidat));
 			for (int i = 0; i < condSize; i++)
 			{
-				printf("entrer prenom %d : ", i);
+				printf("entrer prenom %d : ", i + 1);
 				scanf("%s", &condidats[i].fname);
-				printf("entrer nom %d : ", i);
+				printf("entrer nom %d : ", i + 1);
 				scanf("%s", &condidats[i].lname);
-				printf("entrer cin %d : ", i);
+				printf("entrer cin %d : ", i + 1);
 				scanf("%s", &condidats[i].cin);
 			}
 		}
 	}
-	//system("clear");
-	printf("liste des electeurs:\n");
+	system("clear");
+	//system("cls");
+	printf("liste des electeurs:\n\n");
 	printElects(electeurs, elecSize);
-	printf("liste des condidats:\n");
+	printf("liste des condidats:\n\n");
 	printConds(condidats, condSize);
 
 	// tour 1
@@ -108,18 +111,18 @@ void main()
 		{
 			printf("%s vote pour : ", electeurs[in].cin);
 			scanf("%d", &idx);
-			if (idx < 0 || idx >= condSize)
+			if (idx <= 0 || idx > condSize)
 				printf("veuillez choisir un numero parmi ceux affiches a l'ecran\n");
 			else
 			{
-				tour1[in] = idx;
+				tour1[in] = idx - 1;
 				in++;
 			}
 		}
 
-		for (int i = 0; i < elecSize; i++)
-			printf("%d ", tour1[i]);
-		printf("\n");
+		// for (int i = 0; i < elecSize; i++)
+		// 	printf("%d ", tour1[i]);
+		// printf("\n");
 
 		int score[condSize][2];
 		float pourc[condSize];
@@ -129,38 +132,38 @@ void main()
 			score[i][1] = 0;
 			for (int j = 0; j < elecSize; j++)
 			{
-				printf("tour1[%d] = %d || i = %d\n", j, tour1[j], i);
+				// printf("tour1[%d] = %d || i = %d\n", j, tour1[j], i);
 				if (i == tour1[j])
 					score[i][1]++;
-				printf("%d -> %d\n", score[i][0], score[i][1]);
+				// printf("%d -> %d\n", score[i][0], score[i][1]);
 			}
-			printf("\n");
 		}
-		for (int i = 0; i < elecSize; i++)
-			printf("%d ", tour1[i]);
-		printf("\n");
+		// for (int i = 0; i < elecSize; i++)
+		// 	printf("%d ", tour1[i]);
+		// printf("\n");
 		pass = 0;
 		for (int i = 0; i < condSize; i++)
 		{
 			pourc[i] = (float)score[i][1] * 100 / elecSize;
 			if (pourc[i] > (float)15.0)
 				pass++;
-			printf("%d has %d votes (%.3f%%)\n", score[i][0], score[i][1], pourc[i]);
+			// printf("%s a %d votes (%.3f%%)\n", condidats[i].cin, score[i][1], pourc[i]);
 		}
 		in = 0;
 		int diff = 0;
 		while (diff == 0 && in < condSize - 1)
 		{
-			printf("\npourc[%d] : %f| pourc[%d] : %f", in, pourc[in], in + 1, pourc[in + 1]);
+			// printf("\npourc[%d] : %f| pourc[%d] : %f", in, pourc[in], in + 1, pourc[in + 1]);
 			if (pourc[in] != pourc[in + 1])
 			{
-				printf("\n***diff****\n");
 				diff = 1;
 				break;
 			}
 			in++;
 		}
-		printf("\ndiff = %d | in = %d | pass = %d\n", diff, in, pass);
+		// printf("\ndiff = %d | in = %d | pass = %d\n", diff, in, pass);
+		system("clear");
+		//system("cls");
 		if (diff != 0)
 		{
 			nommT1 = (struct condidat *)malloc(pass * sizeof(struct condidat));
@@ -178,7 +181,18 @@ void main()
 			t = 1;
 		}
 		else
-			printf("**le premier tour sera refait (les condidats ont le meme nombre de votes**\n");
+		{
+			//system("clear");
+			//system("cls");
+			printf("**le premier tour sera refait (les condidats ont le meme nombre de votes**\n\n");
+			printConds(condidats, condSize);
+			printf("\n");
+		}
+		for (int i = 0; i < condSize; i++)
+		{
+			printf("%s a %d votes (%.3f%%)\n", condidats[i].cin, score[i][1], pourc[i]);
+		}
+		printf("\n");
 	}
 	printf("liste des condidats nomines pour le deuxieme tour\n");
 	printConds(nommT1, pass);
@@ -193,17 +207,17 @@ void main()
 		{
 			printf("%s vote pour : ", electeurs[in].cin);
 			scanf("%d", &idx);
-			if (idx < 0 || idx >= condT2)
+			if (idx <= 0 || idx > condT2)
 				printf("veuillez choisir un numero parmi ceux affiches a l'ecran\n");
 			else
 			{
-				tour2[in] = idx;
+				tour2[in] = idx - 1;
 				in++;
 			}
 		}
-		for (int i = 0; i < elecSize; i++)
-			printf("%d ", tour2[i]);
-		printf("\n");
+		// for (int i = 0; i < elecSize; i++)
+		// 	printf("%d ", tour2[i]);
+		// printf("\n");
 		int score[condT2][2];
 		float pourc2[condT2];
 		for (int i = 0; i < condT2; i++)
@@ -235,6 +249,8 @@ void main()
 			}
 			in++;
 		}
+		system("clear");
+		//system("cls");
 		if (diff != 0)
 		{
 			nommT2 = (struct condidat *)malloc(condT2 * sizeof(struct condidat));
@@ -252,7 +268,16 @@ void main()
 			t = 1;
 		}
 		else
+		{
 			printf("**le deuxieme tour sera refait (le condidats ont le meme nombre de votes**\n");
+			printConds(nommT1, condT2);
+			printf("\n");
+		}
+		for (int i = 0; i < condT2; i++)
+		{
+			printf("%s a %d votes (%.3f%%)\n", nommT2[i].cin, score[i][1], pourc2[i]);
+		}
+		printf("\n");
 	}
 	if (pass != 1)
 	{
@@ -268,11 +293,11 @@ void main()
 			{
 				printf("%s vote pour : ", electeurs[in].cin);
 				scanf("%d", &idx);
-				if (idx < 0 || idx >= condT3)
+				if (idx <= 0 || idx > condT3)
 					printf("veuillez choisir un numero parmi ceux affiches a l'ecran\n");
 				else
 				{
-					tour3[in] = idx;
+					tour3[in] = idx - 1;
 					in++;
 				}
 			}
@@ -294,13 +319,13 @@ void main()
 				pourc3[i] = (float)score[i][1] * 100 / elecSize;
 				if (pourc3[i] > (float)15.0)
 					pass++;
-				printf("%d has %d votes (%.3f%%)\n", score[i][0], score[i][1], pourc3[i]);
+				// printf("%d has %d votes (%.3f%%)\n", score[i][0], score[i][1], pourc3[i]);
 			}
 			printf("\npourc\n");
-			for (int i = 0; i < condT3; i++)
-			{
-				printf("i = %d | pourc i = %.3f\n", i, pourc3[i]);
-			}
+			// for (int i = 0; i < condT3; i++)
+			// {
+			// 	printf("i = %d | pourc i = %.3f\n", i, pourc3[i]);
+			// }
 			int maxIdx = 0;
 			for (int i = 1; i < condT3; i++)
 			{
@@ -316,15 +341,23 @@ void main()
 					found++;
 				i++;
 			}
-
+			system("clear");
+			//system("cls");
+			for (int i = 0; i < condT3; i++)
+			{
+				printf("%s a %d votes (%.3f%%)\n", nommT2[i].cin, score[i][1], pourc3[i]);
+			}
+			printf("\n");
 			if (found)
 			{
 				printf("**le troisieme tour sera refait (au moins deux conidat ont le meme nombre de votes**\n");
+				printConds(nommT2, condT3);
+				printf("\n");
 			}
 			else
 			{
-				printf("**le condidat elu a la presidence depuis le troisieme tour : \n");
-				printf("%s\t%s\t%s", nommT2[maxIdx].fname, nommT2[maxIdx].lname, nommT2[maxIdx].cin);
+				printf("**le condidat elu a la presidence depuis le troisieme tour : \n\n");
+				printf("%s\t%s\t%s\n\n", nommT2[maxIdx].fname, nommT2[maxIdx].lname, nommT2[maxIdx].cin);
 				t = 1;
 			}
 		}
